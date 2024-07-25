bac664a2cd6341007d52803fea10a754509ed852
Shawn <125345731+NanSang2000@users.noreply.github.com>
2024-07-26 01:28:34 +1000

    Update index.jsx

--- components/PDFViewer/index.jsx
+++ components/PDFViewer/index.jsx
@@ -1,26 +1,29 @@
-import React, { useState } from 'react'
-import { Document, Page, pdfjs } from 'react-pdf'
-import 'react-pdf/dist/esm/Page/TextLayer.css'
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
-function PDFViewer (props) {
-    const [numPages, setNumPages] = useState(null)
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
 
-    function onDocumentLoadSuccess ({ numPages }) {
-        setNumPages(numPages)
-    }
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-    return (
-        <div>
-            <Document
-                file={props.file}
-                onLoadSuccess={onDocumentLoadSuccess}
-            >
-                {Array.from(new Array(numPages || 0), (el, index) => (
-                    <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-                ))}
-            </Document>
-        </div>
-    )
+function PDFViewer(props) {
+  const [numPages, setNumPages] = useState(null);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <Document
+        file={props.file}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        {Array.from(new Array(numPages || 0), (el, index) => (
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+        ))}
+      </Document>
+    </div>
+  );
 }
 
-export default PDFViewer
+export default PDFViewer;
