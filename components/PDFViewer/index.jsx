ae87e7aaf80649c7e0827f6a34dfffa97b08dec2
Nan <nansang2000@gmail.com>
2024-07-26 03:22:50 +1000

    fix err

--- components/PDFViewer/index.jsx
+++ components/PDFViewer/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Worker, Viewer } from '@react-pdf-viewer/core'
 import '@react-pdf-viewer/core/lib/styles/index.css'
 import '@react-pdf-viewer/default-layout/lib/styles/index.css'
-
 function PDFViewer ({ file }) {
   return (
     <Worker workerUrl={'https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js'}>
