bba8af9b0418e8f39c976d8a7c5f7b4e25258f98
Nan <nansang2000@gmail.com>
2024-07-25 20:15:44 +1000

    Initial commit

---
+++ data/projectdata.ts
@@ -0,0 +1,14 @@
+import { type Project } from '../types'
+
+export const Projects: Project[] = [
+]
+
+// eslint-disable-next-line no-lone-blocks
+{
+  /* name: 'TriangleUI',
+//desc: '直观的 React UI 框架，基于 TS + SCSS + Dumi，字节跳动青训营超级码力奖',
+link: 'https://triangle-ui.netlify.app/',
+banner: '/projects/triangle.png',
+username: 'Triangle-Lab',
+repo: 'triangle-design-project' */
+}
