6fbe1d556b15739629276ee9a2260108d1ea1306
Nan <nansang2000@gmail.com>
2024-07-26 02:08:40 +1000

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
+import '../styles/globals.css'
 import '../styles/TextLayer.css'
 import '../styles/AnnotationLayer.css'
 import '../style.css'
 import React from 'react'
 import localFont from '@next/font/local'
-// Vercel Analytics Vercel 分析
+import { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
+
 const myFont = localFont({ src: './PingFangSC.ttf' })
 
-
-export default function App ({ Component, pageProps }): JSX.Element {
-  return <main className={myFont.className}>
-    <Component {...pageProps} />
-    <Analytics />
-  </main>
+export default function App ({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <main className={myFont.className}>
+      <Component {...pageProps} />
+      <Analytics />
+    </main>
+  )
 }
