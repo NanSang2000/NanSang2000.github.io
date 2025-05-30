a4dd8409a5512958729fb1481dc92db2847fa02d
Nan <nansang2000@gmail.com>
2025-05-30 10:43:12 +0800

    add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,10 @@ import localFont from '@next/font/local'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 
-const myFont = localFont({ src: '../public/fonts/PingFangSC.ttf' })
+const myFont = localFont({
+  src: '../public/fonts/PingFangSC.ttf',
+  display: 'swap'
+})
 
 export default function App ({ Component, pageProps }: AppProps): JSX.Element {
   return (
